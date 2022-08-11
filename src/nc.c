8a1139849180c716bd2e38ca0c9aab21760b6c3a
vcsaturninus <vcsaturninus@protonmail.com>
2022-08-11 20:09:53 +0100

    feat: add unix domain socket support

--- src/nc.c
+++ src/nc.c
@@ -8,47 +8,63 @@
 #include <fcntl.h>       /* open() */
 #include <sys/stat.h>    /* mode_t constants */
 #include <arpa/inet.h>   /* inet_pton */
-
+#include <sys/un.h>      /* struct sockaddr_un */
 #include <errno.h>
 
+/*============
+  -- Macros --
+  ===========*/
 
+/* silence unused variable warning */
 #define UNUSED(x) (void)(x)
 
 /* call perror() to print msg and then exit with error */
-#define perror_exit(msg) do{ perror(msg); exit(EXIT_FAILURE); } while(0);
+#define exit_perror(msg) do{ perror(msg); exit(EXIT_FAILURE); } while(0);
 
+/* print msg to stderr and then exit with error */
+#define exit_print(...) do{ fprintf(stderr, __VA_ARGS__); exit(EXIT_FAILURE); } while(0);
+
+/* Size of accept() backlog */
 #define BACKLOG_SIZE 10
 
-/* Show help message for [-h] */
-void show_usage(char **argv){
-    printf(
-" %s\n"
-"   [-h]\n"
-"   [-f FILE] [-l] ADDRESS PORT\n",
-    argv[0]);
+/* buffer size for connect utility function */
+#define BUFFER_SIZE 8192  /* 2^13; large buffer to speed up transfer */
 
-    fflush(stdout);
-}
+
+/*===================
+  -- Declarations --
+  ==================*/
+
+/* IO failure return codes */
+enum {READ_FAIL=-1, WRITE_FAIL=-2};
+
+/* Show help message for [-h] */
+void show_usage(char **argv);
+
+/* read bytes from src and write to dst until EOF or error */
+int transfer(int src, int dst);
+
 
 
 
 int main(int argc, char *argv[]){
-    int rc = 0;  /* used to store the return code of various functions */
-    int sd = 0;  /* socket descriptor */
-    char buff[8192] = {0};  /* 2^13; large buffer to speed up transfer */
+    int rc = 0;          /* used to store the return code of various functions */
+    int sd = 0;          /* socket descriptor */
 
-    /* flag for running in receiver/server mode */
-    int LISTENER = 0;
-    char *file = NULL;
-    UNUSED(file);       /* only optionally used */
+    int LISTENER = 0;    /* flag for running in receiver/server mode */
+    int UNIX_DOMAIN = 0; /* flag to use unix domain sockets */
 
-    /*===================
-     * -- CLI parsing ---
-     *==================*/
-    const char *optstring = "lhf:";
+    char *file = NULL;   /* optionally specified input/output file */
+    UNUSED(file);
+
+    /*==================
+     * -- CLI parsing --
+     *=================*/
+    const char *optstring = "f:hlu";
     char opt = 0;
 
     extern int optind;
+    extern char *optarg;
 
     while ((opt = getopt(argc, argv, optstring)) != -1){
         switch(opt){
@@ -60,161 +76,220 @@ int main(int argc, char *argv[]){
                 file = optarg;
                 break;
 
+            case 'u':
+                UNIX_DOMAIN = 1;
+                break;
+
             case 'h':
                 show_usage(argv);
                 exit(EXIT_SUCCESS);
                 break;
 
             case '?':
+            case ':':
                 exit(EXIT_FAILURE);
+                break;
         }
     }
 
-    /* 2 positional params expected: ADDRESS and PORT */
-    if (optind != argc-2){
-        fprintf(stderr, "Invalid cli specification; see help message\n");
-        exit(EXIT_FAILURE);
-    }
-    const char *HOST_ADDR = argv[optind];
-    const char *PORT_NUM = argv[optind+1];
-
-
-    /*=====================
-     * -- Socket setup ---
-     *====================*/
-    struct addrinfo hints;
-    struct addrinfo *res = NULL;
-    memset(&hints, 0, sizeof(struct addrinfo));
-
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_family = AF_UNSPEC;  /* ipv4 OR ipv6 */
-    hints.ai_flags = AI_NUMERICHOST | AI_NUMERICSERV; /* args expected to be numeric specs */
-    
-    if ( (rc = getaddrinfo(HOST_ADDR, PORT_NUM, &hints, &res)) != 0 ){
-        fprintf(stderr, "getaddrinfo failure: %s\n", gai_strerror(rc));
-        exit(EXIT_FAILURE);
-    }
-    
-    bool found_sock = false;
-    for (struct addrinfo *i = res; i; i = i->ai_next){
-        sd = socket(i->ai_family, i->ai_socktype, i->ai_protocol);
-
-        if (sd == -1) continue;   /* try another from the list */
-        
-        if (!LISTENER){
-            found_sock = true;
-            break;
+    /* where to read and write from:
+       1) client reads from stdin or file and sends to server via socket
+       2) server reads from socket and writes to stdout or file */
+    int input  = 0; /* stdin */
+    int output = 1; /* stdout */
+    if (file && LISTENER){
+        mode_t perms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+        if ((output = open(file, O_CREAT|O_WRONLY|O_APPEND, perms)) == -1){
+            exit_perror("Failed to open file for writing");
         }
-
-        if (bind(sd, i->ai_addr, i->ai_addrlen) == 0){
-            found_sock = true;
-            break;
+    } else if (file && !LISTENER){
+        if ((input = open(file, O_RDONLY)) == -1){
+            exit_perror("Failed to open file for reading");
         }
-        
-        close(sd); /* bind failure */
     }
 
-    freeaddrinfo(res); // no longer needed
-
-    if (!found_sock){
-        fprintf(stderr, "Failed to set up socket. FATAL.\n");
-        exit(EXIT_FAILURE);
-    }
-     
-    if (LISTENER){ /* server/receiver */
-        if (listen(sd, BACKLOG_SIZE) == -1){
-            fprintf(stderr, "Failed to listen on socket. FATAL.\n");
-            exit(EXIT_FAILURE);
+    /*====================
+     * -- Socket logic --
+     *===================*/
+    if (UNIX_DOMAIN){
+        /* 1 positional param expected: path to unix domain socket */
+        if (argc != optind+1){
+            exit_print("Invalid cli specification; see help message\n");
         }
-        
-        socklen_t addrlen = sizeof(struct sockaddr_storage);
-        struct sockaddr_storage peer_addr = {0};
-        int connsock = accept(sd, (struct sockaddr *)&peer_addr, &addrlen);
+        char *SOCK_PATH = argv[optind];
 
-        if (connsock == -1){
-            perror_exit("accept() error");
+        if (( sd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1){
+            exit_perror("Failed to create Unix Domain socket");
         }
-        
-        /* where the write the sender's message */
-        int outstream = 1; /* stdout */
-        if (file){
-            mode_t perms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-            if ((outstream = open(file, O_CREAT|O_WRONLY|O_APPEND, perms)) == -1){
-                perror_exit("Failed to open file for writing");
+
+        struct sockaddr_un unaddr;
+        memset(&unaddr, 0, sizeof(struct sockaddr_un));
+        unaddr.sun_family = AF_UNIX;
+        strncpy(unaddr.sun_path, SOCK_PATH, sizeof(unaddr.sun_path)-1);
+
+        if (LISTENER){
+            if (unlink(SOCK_PATH) == -1 && errno != ENOENT){
+                fprintf(stderr, "Failed to delete %s : '%s'\n", SOCK_PATH, strerror(errno));
             }
-        }
 
-        int bytes_read = 0;
-        while ( (bytes_read = read(connsock, buff, sizeof(buff))) ){
-            if (bytes_read == -1){
-                perror_exit("Failed to read from socket");
+            if (bind(sd, (struct sockaddr *)&unaddr, sizeof(struct sockaddr_un)) == -1){
+                exit_perror("Failed to bind to socket");
             }
-            
-            if (write(outstream, buff, bytes_read) != bytes_read){
-                // todo: deal with case where errno is simply EINT for partial writes
-                perror_exit("write failure");
-            } /* write */
-        } /* while loop */
-
-        if (file) close(outstream);
-        close(sd);
-    } /* server/receiver */
-
-    else {  /* client, sender */
-        int instream = 0;  
-        if (file){
-            if ((instream = open(file, O_RDONLY)) == -1){
-                perror_exit("Failed to open file for reading");
+            rc = transfer(sd, output);
+        }
+        else{
+            // connect so we can use write instead of send
+            if (connect(sd, (struct sockaddr *)&unaddr, sizeof(struct sockaddr_un)) == -1){
+                exit_perror("Failed to connect");
             }
+            rc = transfer(input, sd);
         }
 
-        struct sockaddr_storage peer;
-        memset(&peer, 0, sizeof(struct sockaddr_storage));
-        socklen_t addrlen = sizeof(struct sockaddr_storage);
-
-        printf("host is %s\n", HOST_ADDR);
-        struct sockaddr_in in;
-        struct sockaddr_in6 in6;
-        memset(&in, 0, sizeof(in));
-        memset(&in6, 0, sizeof(in6));
-
-        // figure out if cmdline specified ipv4 or ipv6 address
-        if (inet_pton(AF_INET, HOST_ADDR, &in.sin_addr) == 1){
-            in.sin_port = htons(strtol(PORT_NUM, NULL, 10));
-            in.sin_family = AF_INET;
-            memcpy(&peer, &in, sizeof(struct sockaddr_in));
-        }
-        else if (inet_pton(AF_INET6, HOST_ADDR, &in6.sin6_addr) == 1){
-            in6.sin6_family = AF_INET6;
-            in6.sin6_port = htons(strtol(PORT_NUM, NULL, 10));
-            memcpy(&peer, &in6, sizeof(struct sockaddr_in));
-        }else{
-            perror_exit("Failed to form peer socket in either AF_INET or AF_INET6");
+        if (rc == READ_FAIL){
+            exit_perror("Read failure");
+        }else if (rc == WRITE_FAIL){
+            exit_perror("Write failure");
         }
 
-        //if (connect(sd, (struct sockaddr *)&peer_addr, addrlen) == -1){
-        if (connect(sd, (struct sockaddr *)&peer, addrlen) == -1){
-            perror_exit("Failed to establish connection with peer");
+        if (LISTENER) unlink(SOCK_PATH);
+    } /* Unix Domain */
+
+    else{ /* Internet Domain (IPv4/v6) */
+
+        /* 2 positional params expected : ADDRESS and PORT */
+        if (argc != optind+2){
+            exit_print("Invalid cli specification; see help message\n");
+        }
+        const char *HOST_ADDR = argv[optind];
+        const char *PORT_NUM = argv[optind+1];
+
+        struct addrinfo hints;
+        struct addrinfo *res = NULL;
+        memset(&hints, 0, sizeof(struct addrinfo));
+
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_family = AF_UNSPEC;  /* ipv4 OR ipv6 */
+        hints.ai_flags = AI_NUMERICHOST | AI_NUMERICSERV; /* args expected to be numeric specs */
+
+        if ( (rc = getaddrinfo(HOST_ADDR, PORT_NUM, &hints, &res)) != 0 ){
+            exit_print("getaddrinfo failure: %s\n", gai_strerror(rc));
         }
 
-        int bytes_read = 0;
-        while ( (bytes_read = read(instream, buff, sizeof(buff))) ){
-            if (bytes_read == -1){
-                perror_exit("Failed to read from stream");
+        bool found_sock = false;
+        for (struct addrinfo *i = res; i; i = i->ai_next){
+            sd = socket(i->ai_family, i->ai_socktype, i->ai_protocol);
+
+            if (sd == -1) continue;   /* try another from the list */
+
+            if (!LISTENER){
+                found_sock = true;
+                break;
             }
-            
-            if (write(sd, buff, bytes_read) != bytes_read){
-                puts("two");
-                // todo: deal with case where errno is simply EINT for partial writes
-                perror_exit("write failure");
-            } /* write */
-        } /* while loop */
 
-        if (file) close(instream);
-        close(sd);
-    } /* client/sender */
+            if (bind(sd, i->ai_addr, i->ai_addrlen) == 0){
+                found_sock = true;
+                break;
+            }
 
-} /* main */
+            close(sd); /* bind failure */
+        }
 
+        freeaddrinfo(res); // no longer needed
+
+        if (!found_sock){
+            exit_print("Failed to set up socket. FATAL.\n");
+        }
+
+        if (LISTENER){ /* server/receiver */
+            if (listen(sd, BACKLOG_SIZE) == -1){
+                exit_print("Failed to listen on socket. FATAL.\n");
+            }
+
+            socklen_t addrlen = sizeof(struct sockaddr_storage);
+            struct sockaddr_storage peer_addr = {0};
+            int connsock = accept(sd, (struct sockaddr *)&peer_addr, &addrlen);
+
+            if (connsock == -1){
+                exit_perror("accept() error");
+            }
+
+            rc = transfer(connsock, output);
+        } /* server/receiver */
+
+        else {  /* client, sender */
+            struct sockaddr_storage peer;
+            socklen_t addrlen = sizeof(struct sockaddr_storage);
+            struct sockaddr_in in;
+            struct sockaddr_in6 in6;
+            memset(&peer, 0, sizeof(struct sockaddr_storage));
+            memset(&in,   0, sizeof(in));
+            memset(&in6,  0, sizeof(in6));
+
+            // figure out if cmdline specified ipv4 or ipv6 address
+            if (inet_pton(AF_INET, HOST_ADDR, &in.sin_addr) == 1){
+                in.sin_port = htons(strtol(PORT_NUM, NULL, 10));
+                in.sin_family = AF_INET;
+                memcpy(&peer, &in, sizeof(struct sockaddr_in));
+            }
+            else if (inet_pton(AF_INET6, HOST_ADDR, &in6.sin6_addr) == 1){
+                in6.sin6_family = AF_INET6;
+                in6.sin6_port = htons(strtol(PORT_NUM, NULL, 10));
+                memcpy(&peer, &in6, sizeof(struct sockaddr_in));
+            }else{
+                exit_perror("Failed to form peer socket in either AF_INET or AF_INET6");
+            }
+
+            if (connect(sd, (struct sockaddr *)&peer, addrlen) == -1){
+                exit_perror("Failed to establish connection with peer");
+            }
+
+            rc = transfer(input, sd);
+        } /* client/sender */
+
+        if (rc == READ_FAIL){
+            exit_perror("Read failure");
+        }else if (rc == WRITE_FAIL){
+            exit_perror("Write failure");
+        }
+    } /* Internet Domain (IPv4/v6) */
+
+    if (file && LISTENER)  close(output);  /* server */
+    if (file && !LISTENER) close(input);   /* client */
+    close(sd);
+} /* main() */
+
+
+
+/*===========================
+  -- Function definitions  --
+  ===========================*/
+void show_usage(char **argv){
+    printf(
+" %s\n"
+"   [-h]\n"
+" \nIPv4/IPv6:\n"
+"   [-f FILE] [-l] <ADDRESS> <PORT>\n"
+" Unix Domain Sockets:\n"
+"   [-f FILE] [-l] <UDS PATH> \n",
+    argv[0]);
+
+    fflush(stdout);
+}
+
+int transfer(int src, int dst){
+    int16_t bytes_read = 0;
+    char buff[BUFFER_SIZE] = {0};
+
+    while ( (bytes_read = read(src, buff, BUFFER_SIZE)) ){
+        if (bytes_read == -1) return READ_FAIL;
+
+        if (write(dst, buff, bytes_read) != bytes_read){
+            // todo: deal with case where errno is simply EINT for partial writes
+            return WRITE_FAIL;
+        } /* write */
+    } /* while loop */
+
+    return 0;
+}
 
 
--- src/nc.h
+++
@@ -1,5 +0,0 @@
-#ifndef NC_HEADER__
-#define NC_HEADER__
-
-
-#endif
